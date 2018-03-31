799f94008ecf2da6d025681aa7e98a013d4d670f
Felipe Pereira <felipepereira2697@gmail.com>
2018-03-31 08:24:54 -0700

    Primeiros exercicios em Python

---
+++ hello.py
@@ -0,0 +1,25 @@
+﻿"""Esse modulo é para o primeiro hello world com python."""
+A = "Ola mundo"
+print(A)
+cash = 19867324678987.99 
+amountPerCapita = cash / 5 
+print(amountPerCapita)
+print("Felipe \t Pereira")
+name = "Felipe"
+
+#interpolation
+print(f"Olá o meu nome é: {name}!!!")
+
+idade = 20
+altura = 1.77
+print("Idade e altura: {} anos e {}".format(idade, altura))
+
+my_list = [1,2,3,4]
+print(str(my_list))
+#pegar o tipo da variavel
+decimal = 12.4444
+
+integer = 1
+
+print(type(name))
+print(type(decimal))
